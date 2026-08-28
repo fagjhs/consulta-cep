dacccc24ad55d9bb94a11d055b943a4c007351d0
Ana Carolina Lanes <anacarolinamonteirolanes@gmail.com>
2026-08-28 18:06:24 -0300

    Valida o formato do CEP antes de consultar

--- consulta_cep.py
+++ consulta_cep.py
@@ -1,5 +1,11 @@
 import requests
 
+def limpar_cep(cep):
+    return cep.replace("-", "").replace(".", ""). strip()
+
+def cep_valido(cep):
+    return cep.isdigit() and len(cep) == 8
+
 def consultar_cep(cep):
     url = f"https://viacep.com.br/ws/{cep}/json/"
     resposta = requests.get(url)
@@ -19,7 +25,10 @@ while True:
     print("2 - Sair")
     opcao = input("Escolha uma opção: ")
     if opcao == "1":
-        cep = input("Digite o CEP (só números): ")
+        cep = limpar_cep(input("Digite o CEP (só número): "))
+        if not cep_valido(cep):
+            print("CEP inválido! Digite 8 números, sem espaços ou traços.")
+            continue
         dados = consultar_cep(cep)
         exibir_endereco(dados)
     elif opcao == "2":
